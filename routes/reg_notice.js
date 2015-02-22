fc7853c4f6b8483490acad95f62f00b8bdcf93bc
zoey <417946@qq.com>
2015-02-23 00:57:07 +0800

    fix bug

---
+++ routes/reg_notice.js
@@ -0,0 +1,63 @@
+/**
+ * Created by zoey on 15-2-16.
+ */
+var db = require('./mysql/dboperator');
+var userInfo = require('./userInfo.js').userInfo;
+var user = require('./user.js');
+var analysis = require('./module/analysis');
+/**
+ * 男女三件事
+ * @param req
+ * @param res
+ */
+exports.onRegNotice = function(req,res){
+    //解析生日
+    var strDate = req.query['birthday'];
+    console.log(strDate)
+    var result = /(\d+).*?(\d+).*?(\d+).*?(\d+)\:(\d+)/g.exec(strDate);
+    //测试功能
+    var reqData = {
+        sex:			parseInt(req.query['sex']),
+        birthAddress:	parseInt(req.query['birthaddress'])-1,
+        year:			parseInt(result[1]),
+        month:			parseInt(result[2]),
+        day:			parseInt(result[3]),
+        clock:			parseInt(result[4])
+    }
+
+    var userInfo = user.getUserInfo(reqData);
+    var ten=[2008,2007,2006,2014,2013,2012,2011,2010,2009];
+    var flag=true;
+    var r={}
+    while(flag){
+        if(userInfo.flystar){
+            flag=false;
+            userInfo.yangSum=userInfo.yangSum1;
+            userInfo.year_star=userInfo.nianyun;
+            r.user=userInfo;
+            db.getBaseXg(userInfo,function(){
+//                res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
+                r.a="您是一位"+userInfo.baseXg;
+                console.log(JSON.stringify(userInfo))
+                analysis.getYun4Yc(userInfo,4,null,false,"yc",function(desc){
+                    r.b="一四年您运程（中等），事业（小衰），在6月左右，"+desc;
+
+                    var highWaiStar=0;//取其余星五行分值(已考虑四季，出生地影响) 最高的一颗外飞星
+                    var tempHighValue=0;
+                    var waiStarList=[userInfo.bigyun,userInfo.smallyun,userInfo.yueyun,userInfo.riyun,userInfo.shiyun]
+                    for(var i in waiStarList){
+                        var temp=userInfo.wwxNum[waiStarList[i]-1];
+                        if(tempHighValue<temp){
+                            tempHighValue=temp;
+                            highWaiStar=waiStarList[i];
+                        }
+                    }
+                    analysis.getYun4Yc(userInfo,8,highWaiStar,null,"yc",function(desc){
+                        r.c="过去10年中，您的运程（顺），事业（中等），在（"+ten[highWaiStar-1]+"）年，"+desc;
+                        res.json(r);
+                    });
+                });
+            })
+        }
+    }
+};
