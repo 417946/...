e0c62a5b7e0f48754d393b6527fb7ec695625e0b
zoey <417946@qq.com>
2015-04-06 14:15:29 +0800

    日期详情

--- routes/index.js
+++ routes/index.js
@@ -47,3 +47,84 @@ exports.index = function(req, res){
 	  
   res.send(gz[(2013-1950+26)%60] + "年" + gz[(days+32)%60] + "日");*/
 };
+var tools = require('./tools/tools')
+var LunarCalendar = require("lunar-calendar");
+var user = require('./user.js');
+/**
+ * 黄帝历：4710年6月5日8点5分
+ 阳历：2014年7月11日8点5分
+ 阴历：2014年6月18日8点5分
+ 干支：甲午（年）辛未（月）癸未（日）丙辰（时）
+ 飞星：阳384382 阴372374（大运、小运、年、月、日、时）
+ 节气：小暑中
+ * @param req
+ * @param res
+*/
+exports.dateDetail = function(req, res){
+//    var date=new Date();
+    var year = req.query.sex;
+    var year = req.query.year;
+    var month = req.query.month;
+    var day = req.query.day;
+    var hour = req.query.hour;
+    var min = req.query.min;
+    var second =date.query.seconds;
+//    var sex = 1;
+//    var year = date.getFullYear();
+//    var month = date.getMonth()+1;
+//    var day = date.getDate();
+//    var hour = date.getHours();
+//    var min = date.getMinutes();
+//    var second =date.getSeconds();
+    if (hour >= 23) {
+        hour = 0;
+    }
+    var ob = new Object();
+    var t = tools.timeStr2hour(hour + ":"+min+":"+second);
+    var jd=tools.JD.JD(tools.year2Ayear(year), month, day+t/24);
+    tools.obb.mingLiBaZi( jd-tools.J2000, 0, ob ); //八字计算
+    //阴历
+    var lunar = LunarCalendar.solarToLunar(year,month,day);
+    var starfly1=getStarfly(new Date(year + "/" + month + "/" + day + " " + hour + ":"+min+":00"),sex)
+    var starfly2=getStarfly(new Date(lunar.lunarYear + "/" + lunar.lunarMonth + "/" + lunar.lunarDay + " " + hour + ":"+min+":00"),sex)
+
+    var result='';
+    result+='阳历：'+year+'年'+month+'月'+day+'日'+hour+'点'+min+'分';
+    result+='阴历：'+lunar.lunarYear+'年'+lunar.lunarMonthName+' '+lunar.lunarDayName+' '+hour+'点'+min+'分';
+    result+='干支：'+ob.bz_jn+'（年）'+ob.bz_jy+'（月）'+ob.bz_jr+'（日）'+ob.bz_js+'（时）';
+    result+='飞星：阳'+starfly1+' 阴'+starfly2+'（大运、小运、年、月、日、时）';
+    if(ob.term){
+        result+='节气：'+ob.term;
+    }
+    if(ob.solarFestival){
+        result+=ob.solarFestival;
+    }
+    if(ob.lunarFestival){
+        result+=ob.lunarFestival;
+    }
+}
+function getStarfly(date,sex){
+    var riyunclock=date.getHours();
+    if(riyunclock==0){
+        riyunclock=1;
+    }
+    var riyundate = new Date(date.getYear() + "/" + (date.getMonth()+1) + "/" + date.getDate() + " " + riyunclock + ":00:00");
+//大小运数 //60年一大运，20年一小运 从1924年2月4日开始 大运2，小运4
+    bigyun = user.getBigStar(date);
+    smallyun = user.getSmallStar(date);
+    nianyun = user.getYearStar(date);
+    //月运数
+    yueyun = user.getMonthStar(date);
+    riyun = user.getDayStar(riyundate);
+    shiyun = user.getClockStar(date);
+    //男女运数区别
+    if(sex == 0){
+        bigyun = user.getNvYun(bigyun);
+        smallyun = user.getNvYun(smallyun);
+        nianyun = user.getNvYun(nianyun);
+        yueyun = user.getNvYun(yueyun);
+        riyun = user.getNvYun(riyun);
+        shiyun = user.getNvYun(shiyun);
+    }
+    return bigyun+''+smallyun+''+nianyun+''+yueyun+''+riyun+''+shiyun;
+}
