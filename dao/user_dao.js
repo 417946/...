92971ccf4ea270c3342b6eb65ffccc057e3698f1
Cadbury <417946@qq.com>
2015-05-23 21:44:55 +0800

    1

--- dao/user_dao.js
+++ dao/user_dao.js
@@ -7,7 +7,7 @@ var common = require("../common.js");
 
 operater.getUserDetailById = function(user_id,cb){
     var values = [user_id];
-    var sql = "select u.user_id uid,u.name,u.bless,d.* from user_table u left join user_detail_table d on u.user_id=d.user_id where u.user_id=? ";
+    var sql = "select u.user_id uid,u.name,u.bless,u.sex,u.birthday,d.* from user_table u left join user_detail_table d on u.user_id=d.user_id where u.user_id=? ";
     console.log(sql);
     mysqlClient.query(sql, values, function (err,res) {
         cb(err,res);
