4a3071875af8bd7516ec8bb5eee0570407f71c6c
zoey <417946@qq.com>
2015-05-25 18:00:57 +0800

    +

---
+++ dao/talk_dao.js
@@ -0,0 +1,26 @@
+/**
+ * Created by zoey on 2015/5/24.
+ */
+var operater = module.exports;
+var mysqlClient = require('../routes/mysql/mysqlclient').init();
+
+
+operater.getFriendList = function(uid,cb){
+    var sql = "select * from talk_friend_table where uid=?";
+    mysqlClient.query(sql, [uid], function (err,res) {
+        cb(err,res);
+    });
+};
+operater.getHistory = function(uid1,uid2,cb){
+    var sql = "SELECT * FROM talk_content_table WHERE (fromUid=? OR toUid=?) AND( fromUid=? OR toUid=?)";
+    mysqlClient.query(sql, [uid1,uid1,uid2,uid2], function (err,res) {
+        cb(err,res);
+    });
+};
+/*
+operater.getHistory = function(uid1,uid2,cb){
+    var sql = "SELECT * FROM talk_content_table WHERE (fromUid=? OR toUid=?) AND( fromUid=? OR toUid=?)";
+    mysqlClient.query(sql, [uid1,uid1,uid2,uid2], function (err,res) {
+        cb(err,res);
+    });
+};*/
