89af7d53f91868651041e1bd55650f3df00dbab8
Cadbury <417946@qq.com>
2015-09-18 11:10:51 +0800

    1

--- app.js
+++ app.js
@@ -140,7 +140,7 @@ app.post('/find_bless', get_bless.onFindNewReceiveBless);//查看该用户的未
 app.post('/get_bless', get_bless.onGetBless);//收取祝福
 app.post('/no_bless', get_bless.noBless);//收取祝福
 app.post('/get_luck', get_luck.onGetLuck);
-//app.get('/reg_notice', reg_notice.onRegNotice);
+app.get('/reg_notice', reg_notice.onRegNotice);
 app.get('/get_suxie', reg_notice.onGetSuxie);
 app.get('/get_suxie_flystar', reg_notice.onGetSuxieByFlystar);
 app.get('/friendjy', reg_notice.onFriendJy);
